a7f1cdd0641e03c79fec565a5a3385704ebb61a0
SUNGSOO PARK <79965168+sungxsoo@users.noreply.github.com>
2022-12-21 13:06:40 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,5 @@
 def solution(k, m, score):
-    num_box = int(len(score)/m)
-    answer = 0
-    
-    score.sort(reverse = True)
-    
-    for i in range(0,num_box):
-        answer = answer + score[i*m+(m-1)]*m      
-    
 
-    return answer
+
+    
+    return sum(sorted(score)[len(score)%m::m]*m)
